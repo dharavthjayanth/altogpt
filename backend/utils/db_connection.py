e6cf0324782b5a30f1ecba1e90e314b3b6d0c510
dharavthjayanth <148214582+dharavthjayanth@users.noreply.github.com>
2025-04-19 15:48:37 +0700

    Create db_connection.py

---
+++ backend/utils/db_connection.py
@@ -0,0 +1,23 @@
+import os
+import psycopg2
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def get_pg_connection():
+    return psycopg2.connect(
+        host=os.getenv("PG_HOST"),
+        port=os.getenv("PG_PORT"),
+        database=os.getenv("PG_DATABASE"),
+        user=os.getenv("PG_USER"),
+        password=os.getenv("PG_PASSWORD")
+    )
+
+
+def get_supabase_client() -> Client:
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+    return create_client(url, key)
