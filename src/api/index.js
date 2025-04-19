29921110d506b94550e8f89a293df249869dfbc6
dharavthjayanth <148214582+dharavthjayanth@users.noreply.github.com>
2025-04-20 01:07:30 +0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'https://altogpt.onrender.com';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
